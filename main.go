f9d804f415252849c964906b462faf0b23161d15
secona <77039267+secona@users.noreply.github.com>
2024-08-08 18:03:04 +0700

    feat: add shorten POST handler

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
+	"regexp"
 )
 
 type NotFoundData struct {
 	Slug string
 }
+
+var slugRegex = regexp.MustCompile(`^[a-zA-Z0-9\-]+$`)
 
 func main() {
 	links := map[string]string{
@@ -19,6 +23,13 @@ func main() {
 
 	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		slug := r.URL.String()[1:]
+
+		if slug == "" {
+			t := template.Must(template.ParseFiles("templates/index.html"))
+			t.Execute(w, nil)
+			return
+		}
+
 		value, ok := links[slug]
 
 		if !ok {
@@ -28,6 +39,27 @@ func main() {
 		}
 
 		http.Redirect(w, r, value, 301)
+	})
+
+	mux.HandleFunc("POST /shorten", func(w http.ResponseWriter, r *http.Request) {
+		slug := r.FormValue("slug")
+
+		if !slugRegex.MatchString(slug) {
+			fmt.Fprintf(w, "Shortened link must only contain alphabets, numbers, and hyphens!")
+			return
+		}
+
+		url := r.FormValue("url")
+
+		_, exists := links[slug]
+
+		if exists {
+			fmt.Fprintf(w, "Shortened link <strong>%s<strong> already exists!", slug)
+			return
+		}
+
+		links[slug] = url
+		fmt.Fprintf(w, "Successfully shortened link!")
 	})
 
 	http.ListenAndServe(":8080", mux)
