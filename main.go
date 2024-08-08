6d94d9968d4f96a42c1b88f24a1cc9ba768f3a06
secona <77039267+secona@users.noreply.github.com>
2024-08-08 17:27:09 +0700

    feat: simple links

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"net/http"
+)
+
+type NotFoundData struct {
+	Slug string
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	links := map[string]string{
+		"google": "https://google.com",
+		"pacil":  "https://cs.ui.ac.id",
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		slug := r.URL.String()[1:]
+		value, ok := links[slug]
+
+		if !ok {
+			t := template.Must(template.ParseFiles("templates/404.html"))
+			t.Execute(w, slug)
+			return
+		}
+
+		http.Redirect(w, r, value, 301)
+	})
+
+	http.ListenAndServe(":8080", mux)
 }
